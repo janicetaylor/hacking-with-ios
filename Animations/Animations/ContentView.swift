88f2cdd16360652db9ebbed6bba21c85b245b1d1
Janice Taylor <notjanicetaylor@gmail.com>
2024-04-16 15:15:53 -0500

    initial commit - Animations

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Janice on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationCount = 1.0
+    
+    var body: some View {
+        Button("tap me") {
+            animationCount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationCount)
+        .animation(.spring(duration: 1, bounce: 0.9), value: animationCount) 
+        .blur(radius: (animationCount-1) * 3)
+    }
+}
+
+#Preview {
+    ContentView()
+}
