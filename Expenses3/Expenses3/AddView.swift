1bc84a8b0d316e9289bdcdb9c49e1da197d8caea
Janice Taylor <notjanicetaylor@gmail.com>
2024-04-16 17:09:12 -0500

    initializing expenses, adding save to add view

--- Expenses3/Expenses3/AddView.swift
+++ Expenses3/Expenses3/AddView.swift
@@ -12,8 +12,9 @@ struct AddView: View {
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
-    var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
     
+    var expenses: Expenses
     let types = ["Business", "Personal"]
     
     var body: some View {
@@ -29,6 +30,13 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code: "USD")).keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss() 
+                }
+            }
         }
     }
 }
--- Expenses3/Expenses3/ContentView.swift
+++ Expenses3/Expenses3/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     var id = UUID()
     let name: String
     let type: String
@@ -16,7 +16,23 @@ struct ExpenseItem: Identifiable {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+    
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
 }
 
 struct ContentView: View {
@@ -28,8 +44,17 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(expenses.items, id: \.id) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack {
+                            Text("\(item.name)")
+                                .font(.headline)
+                            Text("\(item.type)")
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }.onDelete(perform:removeItems)
+                
             }
             .navigationTitle("Expenses")
             .toolbar {
