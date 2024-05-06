78f9b39ffde5540b7b40bfb0823545b72da54aa1
Janice Taylor <notjanicetaylor@gmail.com>
2024-05-06 14:38:12 -0500

    updating

--- JSONExample2/JSONExample2/view/ContentView.swift
+++ JSONExample2/JSONExample2/view/ContentView.swift
@@ -13,16 +13,22 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            List {
-                ForEach(userFetcher.users) { user in
-                    VStack(alignment: .leading) {
-                        Text("name: \(user.name)")
-                        Text("username: \(user.username)")
-                        Text("email: \(user.email)")
-                        Text("address: \(user.address.street) \(user.address.suite) \(user.address.city) \(user.address.zipcode) \(user.address.zipcode)")
-                        Text("phone: \(user.phone)")
-                        Text("website: \(user.website)")
+            NavigationStack() {
+                List {
+                    ForEach(userFetcher.users) { user in
+                        VStack(alignment: .leading) {
+                            Text("name: \(user.name)")
+                            Text("username: \(user.username)")
+                            Text("email: \(user.email)")
+                            Text("address: \(user.address.street) \(user.address.suite) \(user.address.city) \(user.address.zipcode) \(user.address.zipcode)")
+                            Text("phone: \(user.phone)")
+                            Text("website: \(user.website)")
+                            NavigationLink("website", value: "\(user.website)")
+                        }
                     }
+                }
+                .navigationDestination(for: String.self) { value in
+                    Text("user website: \(value)")
                 }
             }
         }
