e66505fa41294f8eb789ae2a93b01ceec277af74
Janice Taylor <notjanicetaylor@gmail.com>
2024-06-19 13:19:10 -0400

    binding example

---
+++ Bookworm2/Bookworm2/Bookworm2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm2App.swift
+//  Bookworm2
+//
+//  Created by Janice on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
