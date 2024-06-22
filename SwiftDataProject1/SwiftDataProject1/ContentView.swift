93202a36c34843ded8fda71d783a6a6598bfd962
Janice Taylor <notjanicetaylor@gmail.com>
2024-06-22 11:29:47 -0400

    more filtering

--- SwiftDataProject1/SwiftDataProject1/ContentView.swift
+++ SwiftDataProject1/SwiftDataProject1/ContentView.swift
@@ -5,51 +5,57 @@
 //  Created by Janice on 6/22/24.
 //
 
+import SwiftData 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
     
     @Environment(\.modelContext) var modelContext
-    @Query(filter: #Predicate<User> { user in
-        if user.name.localizedStandardContains("R") {
-              if user.city == "London" {
-                  return true
-              } else {
-                  return false
-              }
-          } else {
-              return false
-          }
-    }, sort: \User.name)
-    var users: [User]
-    @State private var path = [User]()
+    @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
     
     var body: some View {
-        NavigationStack(path: $path) {
-            List(users) { user in
-                NavigationLink(value: user) {
-                    Text(user.name)
+        NavigationStack {
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
+                .navigationTitle("Users")
+                .navigationDestination(for: User.self) { user in
+                    EditUserView(user: user)
                 }
-            }
-            .navigationTitle("Users")
-            .navigationDestination(for: User.self) { user in
-                EditUserView(user: user)
-            }
-            .toolbar {
-                Button("Add Samples", systemImage: "plus") {
+                .toolbar {
+                    Button("Add Samples", systemImage: "plus") {
+                        
+                        try? modelContext.delete(model: User.self)
+                        
+                        let first = User(name: "Ed Sheeran", city: "London", joinDate: .now)
+                        let second = User(name: "Miranda Lambert", city: "Houston", joinDate: .now)
+                        let third = User(name: "Blake Shelton", city: "Los Angeles", joinDate: .now)
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                    }
                     
-                    try? modelContext.delete(model: User.self)
-                    
-                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now)
-                    let second = User(name: "Miranda Lambert", city: "Houston", joinDate: .now)
-                    let third = User(name: "Blake Shelton", city: "Los Angeles", joinDate: .now)
-                    modelContext.insert(first)
-                    modelContext.insert(second)
-                    modelContext.insert(third)
-                    
+                    Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                        showingUpcomingOnly.toggle()
+                    }
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("sort by name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate)
+                                ])
+                            
+                            Text("sort by join date")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name)
+                                ])
+                        }
+                    }
                 }
-            }
         }
     }
 }
---
+++ SwiftDataProject1/SwiftDataProject1/UsersView.swift
@@ -0,0 +1,32 @@
+//
+//  UsersView.swift
+//  SwiftDataProject1
+//
+//  Created by Janice on 6/22/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct UsersView: View {
+    @Query var users: [User]
+    
+    var body: some View {
+        List(users) { user in
+            NavigationLink(value: user) {
+                Text(user.name)
+            }
+        }
+    }
+    
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: \User.name)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
+}
