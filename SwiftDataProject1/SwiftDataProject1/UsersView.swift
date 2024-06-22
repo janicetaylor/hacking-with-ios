4978386b48c247db5cbd08b534bbe9046fd3aca9
Janice Taylor <notjanicetaylor@gmail.com>
2024-06-22 12:42:19 -0400

    relationships

--- SwiftDataProject1/SwiftDataProject1/UsersView.swift
+++ SwiftDataProject1/SwiftDataProject1/UsersView.swift
@@ -10,19 +10,39 @@ import SwiftUI
 
 struct UsersView: View {
     @Query var users: [User]
+    @Environment(\.modelContext) var modelContext
     
     var body: some View {
         List(users) { user in
             NavigationLink(value: user) {
-                Text(user.name)
+                HStack {
+                    Text(user.name)
+                    
+                    Spacer()
+                    
+                    Text(String(user.jobs.count))
+                        
+                }
             }
         }
+        .onAppear(perform: addSample)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: \User.name)
+    }
+    
+    func addSample() {
+        let user = User(name: "Piper Chapman", city: "Houston", joinDate: .now)
+        let job1 = Job(name: "Organize books", priority: 3)
+        let job2 = Job(name: "Do laundry", priority: 2)
+        
+        modelContext.insert(user)
+        
+        user.jobs.append(job1)
+        user.jobs.append(job2)
     }
 }
 
