a4d45b084dbafa7cd0105a1cd8fc32aee2381b06
Janice Taylor <notjanicetaylor@gmail.com>
2024-06-25 11:22:33 -0400

    initial commit

--- Instafilter1/Instafilter1/ContentView.swift
+++ Instafilter1/Instafilter1/ContentView.swift
@@ -8,15 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change filter", action: changeFilter)
+                    Spacer()
+                    
+                    // share the picture
+                }
+            }
+            .navigationTitle("Instafilter")
+            .padding([.horizontal, .bottom])
         }
-        .padding()
+        
     }
+    
+    func changeFilter() {
+        
+    }
+    
+   
 }
 
 #Preview {
