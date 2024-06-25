f14712d4fa3904d4d5aa5186d39aad0ba23fd299
Janice Taylor <notjanicetaylor@gmail.com>
2024-06-25 11:22:20 -0400

    initial commit

---
+++ Instafilter1/Instafilter1/Instafilter1App.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter1App.swift
+//  Instafilter1
+//
+//  Created by Janice on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Instafilter1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
