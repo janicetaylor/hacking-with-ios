a4fcf6cef8c44c1dc9a72f3b2e098eb8f884fc19
Janice Taylor <notjanicetaylor@gmail.com>
2024-06-28 18:18:14 -0400

    updating

---
+++ BucketList1/BucketList1/BucketList1App.swift
@@ -0,0 +1,17 @@
+//
+//  BucketList1App.swift
+//  BucketList1
+//
+//  Created by Janice on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketList1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList1/BucketList1/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  BucketList1
+//
+//  Created by Janice on 6/25/24.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    ContentView()
+}
